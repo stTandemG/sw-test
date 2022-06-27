fb2359665a2866b8f2e1c554f69fc60748acc27c
stimokhin <stimokhin@aligntech.com>
2022-06-27 09:33:36 +0300

    first

--- src/index.js
+++ src/index.js
@@ -12,10 +12,30 @@ root.render(
   </React.StrictMode>
 );
 
+const registerServiceWorker = async () => {
+    if ('serviceWorker' in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register('/sw-test/sw.js', {
+                scope: '/sw-test/'
+            });
+            if (registration.installing) {
+                console.log('Service worker installing');
+            } else if (registration.waiting) {
+                console.log('Service worker installed');
+            } else if (registration.active) {
+                console.log('Service worker active');
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+// serviceWorkerRegistration.register();
+registerServiceWorker().then(() => {})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
